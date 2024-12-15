1c6c88912cd2e0cf9482cbb13c3f92fcbab77fc7
Josue <josue.gonzalez.aguero@hotmail.com>
2024-12-14 19:31:48 -0500

    cooldowns attempt

--- Project/Assets/Scripts/General/EventBus.cs
+++ Project/Assets/Scripts/General/EventBus.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class EventBus
 {
@@ -9,7 +11,7 @@ public class EventBus
     {
         if (_events.TryGetValue(typeof(T), out var handlers))
         {
-            foreach (var handler in handlers)
+            foreach (var handler in handlers.ToList())
             {
                 handler(eventData);
             }
@@ -24,5 +26,18 @@ public class EventBus
             _events[typeof(T)] = handlers;
         }
         handlers.Add((obj) => handler((T)obj));
+    }
+
+    public void Unsubscribe<T>(Action<T> handler)
+    {
+        Debug.Log("Unsubscribing " + handler);
+        if (_events.TryGetValue(typeof(T), out var handlers))
+        {
+            handlers.RemoveAll(h => h == (Action<object>)(obj => handler((T)obj)));
+            if (handlers.Count == 0)
+            {
+                _events.Remove(typeof(T));
+            }
+        }
     }
 }
--- Project/Assets/Scripts/Player/Power Modules/CirclePowerModule.cs
+++ Project/Assets/Scripts/Player/Power Modules/CirclePowerModule.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace PlayerSystem
@@ -7,6 +8,10 @@ namespace PlayerSystem
         private EventBus eventBus;
         private PlayerState playerState;
         private Rigidbody2D rb2d;
+
+        private readonly float maxPowerDuration = 0.5f;
+        private float currentPowerTime = 0f;
+        private float cooldownTimeLeft = 0f;
 
         public CirclePowerModule(EventBus eventBus, PlayerState playerState, Rigidbody2D rb2d, TriggerEventHandler leftTrigger, TriggerEventHandler rightTrigger)
         {
@@ -19,9 +24,30 @@ namespace PlayerSystem
 
         private void togglePower(CirclePowerInputEvent e)
         {
+            if (cooldownTimeLeft > 0f) return;
             int facingDirectionInt = playerState.facingDirection == Direction.Right ? 1 : -1;
             rb2d.velocity = new Vector2(10f * facingDirectionInt, 0f);
+            currentPowerTime = 0f;
+            cooldownTimeLeft = 2f;
+            eventBus.Subscribe<UpdateEvent>(timeoutPower);
+            // eventBus.Subscribe<UpdateEvent>(reducePowerCooldown);
             eventBus.Publish(new ToggleCirclePowerEvent(true, playerState.facingDirection));
+        }
+
+        private void timeoutPower(UpdateEvent e)
+        {
+            currentPowerTime += Time.deltaTime;
+
+            if (currentPowerTime < maxPowerDuration) return;
+            eventBus.Unsubscribe<UpdateEvent>(timeoutPower);
+            eventBus.Publish(new ToggleCirclePowerEvent(false, playerState.facingDirection));
+        }
+
+        private void reducePowerCooldown(UpdateEvent e)
+        {
+            // cooldownTimeLeft -= Time.deltaTime;
+            // if (cooldownTimeLeft > 0f) return;
+            // eventBus.Unsubscribe<UpdateEvent>(reducePowerCooldown);
         }
     }
 }
