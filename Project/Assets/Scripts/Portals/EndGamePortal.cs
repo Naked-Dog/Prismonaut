09c53278ef91a62b06ba5e48ca4326a5b1c0e530
Jorge Sakaino <jorgesakaino@gmail.com>
2025-01-11 20:46:36 -0500

    end game scene implementation

--- Project/Assets/Scripts/Portals/EndGamePortal.cs
+++ Project/Assets/Scripts/Portals/EndGamePortal.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using DG.Tweening;
+
 
 public class EndGamePortal : MonoBehaviour, IPlayerPowerInteractable
 {
     private AudioManager audioManager;
-    [SerializeField] private Collider2D collider2D;
     [SerializeField] private GameObject player3DModel;
 
 
@@ -14,14 +16,18 @@ public class EndGamePortal : MonoBehaviour, IPlayerPowerInteractable
         if (playerState.activePower == PlayerSystem.Power.Circle)
         {
             Debug.Log("EndGame");
-            EnterEndGamePortal(playerState.playerGameObject.transform.position);
-            collider2D.isTrigger = true;
+            playerState.playerGameObject.transform.GetChild(1).gameObject.SetActive(false);
+
+            StartCoroutine(EnterEndGamePortal(playerState.playerGameObject.transform.position));
         }
     }
 
-    private void EnterEndGamePortal(Vector3 playerPosition)
+    private IEnumerator EnterEndGamePortal(Vector3 playerPosition)
     {
         GameObject model3D = Instantiate(player3DModel, playerPosition, Quaternion.identity);
         model3D.transform.localScale = Vector3.one * 0.5f;
+        yield return model3D.transform.DOMove(gameObject.transform.position, 0.5f).WaitForCompletion();
+        yield return model3D.transform.DOScale(Vector3.zero, 1f).WaitForCompletion();
+        MenuController.Instance.ChangeScene("NewAdventuresScene");
     }
 }
---
+++ Project/Assets/Scripts/UI/EndSceneController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+
+public class EndSceneController : MonoBehaviour
+{
+
+    [SerializeField] private Image logo;
+    [SerializeField] private Image message;
+
+    void Start()
+    {
+        StartCoroutine(StartScene());
+    }
+
+    private IEnumerator FadeInImage(Image image)
+    {
+        image.gameObject.SetActive(true);
+        Tween fadeIn = image.DOFade(1, 3f);
+        yield return fadeIn.WaitForCompletion();
+    }
+
+    private IEnumerator StartScene()
+    {
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(FadeInImage(logo));
+        yield return new WaitForSeconds(1.5f);
+        StartCoroutine(FadeInImage(message));
+        yield return new WaitForSeconds(6f);
+        MenuController.Instance.ChangeScene("Menu");
+    }
+}
