583ba24b27f8e02d99c92f54c38cc40caf1431a9
Gianfra100 <gianfra2400@gmail.com>
2025-05-12 19:13:10 -0500

    sounds

---
+++ Project/Assets/Scripts/Player/Animation/BullAnimationEvents.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class BullAnimationEvents : MonoBehaviour
+{
+    private BullSoundsEnum[] stepSounds = new[]
+    {
+        BullSoundsEnum.Step1,
+        BullSoundsEnum.Step2,
+        BullSoundsEnum.Step3,
+    };
+
+    public void PlayStepSound()
+    {
+        var randomSound = stepSounds[Random.Range(0, stepSounds.Length)];
+        AudioManager.Instance.Play2DSound(randomSound, 0.5f, false);
+    }
+
+    public void PlayRushChargeSound()
+    {
+        AudioManager.Instance.Play2DSound(BullSoundsEnum.RushCharge, 0.5f, false);
+    }
+
+    public void PlayRushLoopSound()
+    {
+        AudioManager.Instance.Play2DSound(BullSoundsEnum.LoopRush, 0.5f, false);
+    }
+
+    public void StopRushLoopSound()
+    {
+        AudioManager.Instance.Stop(BullSoundsEnum.LoopRush);
+    }
+
+    public void PlayDetectedSound()
+    {
+        AudioManager.Instance.Play2DSound(BullSoundsEnum.Detected, 0.5f, false);
+    }
+
+    public void PlayHurtSound()
+    {
+        AudioManager.Instance.Play2DSound(BullSoundsEnum.Hurt, 0.5f, false);
+    }
+
+    public void PlayDeathSound()
+    {
+        AudioManager.Instance.Play2DSound(BullSoundsEnum.Death, 0.5f, false);
+    }
+}
