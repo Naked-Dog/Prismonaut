761dec881b8e909a56be2d04ec27f3b9aa1aa89a
Jorge <jorge.luis15520@gmail.com>
2025-05-12 21:16:08 -0500

    final scene event

--- Project/Assets/Scripts/StarShip.cs
+++ Project/Assets/Scripts/StarShip.cs
@@ -1,14 +1,10 @@
+using Unity.AppUI.UI;
 using UnityEngine;
 
 public class StarShip : MonoBehaviour
 {
-    [SerializeField] private string sceneName;
-    private void OnTriggerEnter(Collider other){
-        if(other.CompareTag("Player")){
-            if(MenuController.Instance){
-                MenuController.Instance.ChangeScene(sceneName);
-            }
-            Debug.Log("Cambio de Scena");
-        }
+    public void ChangeScene()
+    {
+        MenuController.Instance.ChangeScene("FinalScene");
     }
 }
